24d094d3af651e7edb862fedc1f519eb10c2c7bb
Hellvinter <45991026+Hellvinter@users.noreply.github.com>
2019-07-27 12:31:58 +0300

    Updates

---
+++ precache-manifest.d09058e32832c12a8def47ed3a8eb314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22273a2c7c26cb0066c4871012d3e5c",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "ed2dcbbd9b166a65cbb2",
+    "url": "/ReactTodoApp/static/css/main.384c048c.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/ReactTodoApp/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "ed2dcbbd9b166a65cbb2",
+    "url": "/ReactTodoApp/static/js/main.8774ad7a.chunk.js"
+  },
+  {
+    "revision": "fcfe4318834a1472d029",
+    "url": "/ReactTodoApp/static/js/runtime~main.a539cb81.js"
+  }
+]);
